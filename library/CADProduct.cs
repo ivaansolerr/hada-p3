83a059d314569e397a143ab0e2af4d76809e6f63
Arid3n <andresradoo@gmail.com>
2025-03-13 19:06:20 +0100

    17611989S created database and the classes in library

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace library
 {
-    public class Class1
+    public class CADProduct
     {
+        private string constring
+        {  get; set; }
+
+        public CADProduct()
+        {
+
+        }
     }
 }
---
+++ library/ENProduct.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    internal class ENProduct
+    {
+        private string _code;
+        private string _name;
+        private int _amount;
+        private float _price;
+        private DateTime _creationDate;
+
+        public string Code
+            { get { return _code; } set { _code = value; } }
+        public string Name
+            { get { return _name; } set {_name = value; } }
+        public int Amount
+            { get { return _amount; } set { _amount = value; } }
+        public float Price
+            { get { return _price; } set { _price = value; } }
+        public DateTime CreationDate
+            { get { return _creationDate; } set { _creationDate = value; } }
+
+        public ENProduct()
+        {
+            Code = string.Empty;
+            Name = string.Empty;
+            Amount = 0;
+            Price = 0;
+            CreationDate = DateTime.MinValue;
+        }
+
+        public ENProduct(string code, string name, int amount, float price, DateTime creationDate)
+        {
+            Code = code;
+            Name = name;
+            Amount = amount;
+            Price = price;
+            CreationDate = creationDate;
+            Code = code;
+            Name = name;
+            Amount = amount;
+            Price = price;
+            CreationDate = creationDate;
+        }
+
+        public bool Create()
+        {
+            if (CADProduct.Create(this)) return true;
+            return false;
+        }
+
+        public bool Update()
+        {
+            if (CADProduct.Update(this)) return true;
+            return false;
+        }
+
+        public bool Delete()
+        {
+            if (CADProduct.Delete(this)) return true;
+            return false;
+        }
+
+        public bool Read()
+        {
+            if (CADProduct.Read(this)) return true;
+            return false;
+        }
+
+        public bool ReadFirst()
+        {
+            if (CADProduct.ReadFirst(this)) return true;
+            return false;
+        }
+
+        public bool ReadNext()
+        {
+            if (CADProduct.ReadNext(this)) return true;
+            return false;
+        }
+
+        public bool ReadPrev()
+        {
+            if (CADProduct.ReadPrev(this)) return true;
+            return false;
+        }
+    }
+}
