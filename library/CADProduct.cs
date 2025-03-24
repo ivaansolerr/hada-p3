3700b529241d8fb07f2c6fb315a67af92f78d0e6
ivaansolerr <ivansoler33@gmail.com>
2025-03-24 22:02:33 +0100

    74530257E constructor cad

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -5,19 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Reflection;
 
 namespace library
 {
     public class CADProduct
     {
-        private string constring = ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString;
+        private string constring;
         public string _constring
         {
             get { return constring; }
         }
         public CADProduct()
         {
-            
+            this.constring = ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString;
+            SqlConnection myDb = new SqlConnection(constring);
+            using (myDb)
+            {
+                myDb.Open(); // idk if this has to be done in every method, I think so
+            }
+        }
+
+        public bool Create(ENProduct product)
+        {
+
+            return true;
         }
     }
 }
