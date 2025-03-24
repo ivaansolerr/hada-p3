b123cbc0653253ba134d4b58c9a3cf29430696b0
Ariden <andresradoo@gmail.com>
2025-03-24 20:26:47 +0100

    17611989S updated CADProducts.cs

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace library
 {
     public class CADProduct
     {
-        private string constring
-        {  get; set; }
-
+        private string constring = ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString;
+        public string _constring
+        {
+            get { return constring; }
+        }
         public CADProduct()
         {
-
+            
         }
     }
 }
