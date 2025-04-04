0e6b8879e46f10d92546d3f05cb810f2ec400502
ivaansolerr <ivansoler33@gmail.com>
2025-04-04 12:49:17 +0200

    74530257E debug

--- proWeb/Default-.aspx.cs
+++ proWeb/Default-.aspx.cs
@@ -14,7 +14,29 @@ namespace proWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // categories
+            CADCategory cADCategory = new CADCategory();
+
+            for (int i = 1; i <= 4; i++)
+            {
+                ENCategory newC = new ENCategory();
+                newC.Id = i;
+                cADCategory.read(newC);
+            }
+
+            List<ENCategory> list = new List<ENCategory>();
+
+            list = cADCategory.readAll();
+
+            if (ddlCategory.Items.Count > 0)
+            {
+                ddlCategory.Items.Clear();
+            }
+
+            foreach (ENCategory c in list)
+            {
+                ddlCategory.Items.Add(c.Name);
+            }
+
         }
         protected void btnCreate_Click(object sender, EventArgs e)
         {
@@ -27,14 +49,21 @@ namespace proWeb
 
             string cat = ddlCategory.Text;
 
-            if (cat == "Computing") newProduct.Category = 0;
-            if (cat == "Telephony") newProduct.Category = 1;
-            if (cat == "Gaming") newProduct.Category = 2;
-            if (cat == "Home appliances") newProduct.Category = 3;
+            if (cat == "Computing") newProduct.Category = 1;
+            if (cat == "Telephony") newProduct.Category = 2;
+            if (cat == "Gaming") newProduct.Category = 3;
+            if (cat == "Home appliances") newProduct.Category = 4;
 
             newProduct.CreationDate = DateTime.Parse(txtCreationDate.Text);
 
             newProduct.Create();
+
+            txtCode.Text = null;
+            txtName.Text = null;
+            txtAmount.Text = null;
+            txtPrice.Text = null;
+            ddlCategory.Text = null;
+            txtCreationDate.Text = null;
         }
 
         protected void btnUpdate_Click(object sender, EventArgs e)
@@ -56,6 +85,13 @@ namespace proWeb
             newProduct.CreationDate = DateTime.Parse(txtCreationDate.Text);
 
             newProduct.Update();
+
+            txtCode.Text = null;
+            txtName.Text = null;
+            txtAmount.Text = null;
+            txtPrice.Text = null;
+            ddlCategory.Text = null;
+            txtCreationDate.Text = null;
         }
 
         protected void btnDelete_Click(object sender, EventArgs e)
@@ -63,20 +99,15 @@ namespace proWeb
             ENProduct newProduct = new ENProduct();
 
             newProduct.Code = txtCode.Text;
-            newProduct.Name = txtName.Text;
-            newProduct.Amount = int.Parse(txtAmount.Text);
-            newProduct.Price = float.Parse(txtPrice.Text);
-
-            string cat = ddlCategory.Text;
-
-            if (cat == "Computing") newProduct.Category = 0;
-            if (cat == "Telephony") newProduct.Category = 1;
-            if (cat == "Gaming") newProduct.Category = 2;
-            if (cat == "Home appliances") newProduct.Category = 3;
-
-            newProduct.CreationDate = DateTime.Parse(txtCreationDate.Text);
 
             newProduct.Delete();
+
+            txtCode.Text = null;
+            txtName.Text = null;
+            txtAmount.Text = null;
+            txtPrice.Text = null;
+            ddlCategory.Text = null;
+            txtCreationDate.Text = null;
         }
 
         protected void btnRead_Click(object sender, EventArgs e)
@@ -84,41 +115,42 @@ namespace proWeb
             ENProduct newProduct = new ENProduct();
 
             newProduct.Code = txtCode.Text;
-            newProduct.Name = txtName.Text;
-            newProduct.Amount = int.Parse(txtAmount.Text);
-            newProduct.Price = float.Parse(txtPrice.Text);
-
-            string cat = ddlCategory.Text;
-
-            if (cat == "Computing") newProduct.Category = 0;
-            if (cat == "Telephony") newProduct.Category = 1;
-            if (cat == "Gaming") newProduct.Category = 2;
-            if (cat == "Home appliances") newProduct.Category = 3;
-
-            newProduct.CreationDate = DateTime.Parse(txtCreationDate.Text);
 
             newProduct.Read();
+
+            txtName.Text = newProduct.Name;
+            txtAmount.Text = newProduct.Amount.ToString();
+            txtPrice.Text = newProduct.Price.ToString();
+
+            int cat = newProduct.Category;
+
+            if (cat == 0) ddlCategory.Text = "Computing";
+            if (cat == 1) ddlCategory.Text = "Telephony";
+            if (cat == 2) ddlCategory.Text = "Gaming";
+            if (cat == 3) ddlCategory.Text = "Home appliances";
+
+            txtCreationDate.Text = newProduct.CreationDate.ToString("yyyy-MM-dd");
         }
 
         protected void btnReadFirst_Click(object sender, EventArgs e)
         {
             ENProduct newProduct = new ENProduct();
 
-            newProduct.Code = txtCode.Text;
-            newProduct.Name = txtName.Text;
-            newProduct.Amount = int.Parse(txtAmount.Text);
-            newProduct.Price = float.Parse(txtPrice.Text);
-
-            string cat = ddlCategory.Text;
-
-            if (cat == "Computing") newProduct.Category = 0;
-            if (cat == "Telephony") newProduct.Category = 1;
-            if (cat == "Gaming") newProduct.Category = 2;
-            if (cat == "Home appliances") newProduct.Category = 3;
-
-            newProduct.CreationDate = DateTime.Parse(txtCreationDate.Text);
-
             newProduct.ReadFirst();
+
+            txtCode.Text = newProduct.Code.ToString();
+            txtName.Text = newProduct.Name;
+            txtAmount.Text = newProduct.Amount.ToString();
+            txtPrice.Text = newProduct.Price.ToString();
+
+            int cat = newProduct.Category;
+
+            if (cat == 0) ddlCategory.Text = "Computing";
+            if (cat == 1) ddlCategory.Text = "Telephony";
+            if (cat == 2) ddlCategory.Text = "Gaming";
+            if (cat == 3) ddlCategory.Text = "Home appliances";
+
+            txtCreationDate.Text = newProduct.CreationDate.ToString("yyyy-MM-dd");
         }
 
         protected void btnReadPrev_Click(object sender, EventArgs e)
@@ -126,20 +158,22 @@ namespace proWeb
             ENProduct newProduct = new ENProduct();
 
             newProduct.Code = txtCode.Text;
-            newProduct.Name = txtName.Text;
-            newProduct.Amount = int.Parse(txtAmount.Text);
-            newProduct.Price = float.Parse(txtPrice.Text);
-
-            string cat = ddlCategory.Text;
-
-            if (cat == "Computing") newProduct.Category = 0;
-            if (cat == "Telephony") newProduct.Category = 1;
-            if (cat == "Gaming") newProduct.Category = 2;
-            if (cat == "Home appliances") newProduct.Category = 3;
-
-            newProduct.CreationDate = DateTime.Parse(txtCreationDate.Text);
 
             newProduct.ReadPrev();
+
+            txtCode.Text = newProduct.Code.ToString();
+            txtName.Text = newProduct.Name;
+            txtAmount.Text = newProduct.Amount.ToString();
+            txtPrice.Text = newProduct.Price.ToString();
+
+            int cat = newProduct.Category;
+
+            if (cat == 0) ddlCategory.Text = "Computing";
+            if (cat == 1) ddlCategory.Text = "Telephony";
+            if (cat == 2) ddlCategory.Text = "Gaming";
+            if (cat == 3) ddlCategory.Text = "Home appliances";
+
+            txtCreationDate.Text = newProduct.CreationDate.ToString("yyyy-MM-dd");
         }
 
         protected void btnReadNext_Click(object sender, EventArgs e)
@@ -147,20 +181,22 @@ namespace proWeb
             ENProduct newProduct = new ENProduct();
 
             newProduct.Code = txtCode.Text;
-            newProduct.Name = txtName.Text;
-            newProduct.Amount = int.Parse(txtAmount.Text);
-            newProduct.Price = float.Parse(txtPrice.Text);
-
-            string cat = ddlCategory.Text;
-
-            if (cat == "Computing") newProduct.Category = 0;
-            if (cat == "Telephony") newProduct.Category = 1;
-            if (cat == "Gaming") newProduct.Category = 2;
-            if (cat == "Home appliances") newProduct.Category = 3;
-
-            newProduct.CreationDate = DateTime.Parse(txtCreationDate.Text);
 
             newProduct.ReadNext();
+
+            txtCode.Text = newProduct.Code;
+            txtName.Text = newProduct.Name;
+            txtAmount.Text = newProduct.Amount.ToString();
+            txtPrice.Text = newProduct.Price.ToString();
+
+            int cat = newProduct.Category;
+
+            if (cat == 0) ddlCategory.Text = "Computing";
+            if (cat == 1) ddlCategory.Text = "Telephony";
+            if (cat == 2) ddlCategory.Text = "Gaming";
+            if (cat == 3) ddlCategory.Text = "Home appliances";
+
+            txtCreationDate.Text = newProduct.CreationDate.ToString("yyyy-MM-dd");
         }
     }
 }
